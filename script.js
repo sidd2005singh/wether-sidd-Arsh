61ec6438b40b807d8d8a5177f333c0d2ad48f4ca
Siddharth Singh <153928349+sidd2005singh@users.noreply.github.com>
2025-06-11 21:38:31 +0530

    afq

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-const messageForm = document.getElementById('messageForm');
-const messageInput = document.getElementById('messageInput');
-const messageContainer = document.getElementById('messageContainer');
+const url =
+	'https://api.openweathermap.org/data/2.5/weather';
+const apiKey =
+	'f00c38e0279b7bc85480c3fe775d518c';
 
-messageForm.addEventListener('submit', (e) => {
-	e.preventDefault();
-	const message = messageInput.value.trim();
-	if (message.length > 0) {
-		const newMessage = document.createElement('div');
-		newMessage.classList.add('message');
-		newMessage.textContent = message;
-		messageContainer.appendChild(newMessage);
-		messageInput.value = '';
-	}
+$(document).ready(function () {
+	weatherFn('Pune');
 });
+
+async function weatherFn(cName) {
+	const temp =
+		`${url}?q=${cName}&appid=${apiKey}&units=metric`;
+	try {
+		const res = await fetch(temp);
+		const data = await res.json();
+		if (res.ok) {
+			weatherShowFn(data);
+		} else {
+			alert('City not found. Please try again.');
+		}
+	} catch (error) {
+		console.error('Error fetching weather data:', error);
+	}
+}
+
+function weatherShowFn(data) {
+	$('#city-name').text(data.name);
+	$('#date').text(moment().
+		format('MMMM Do YYYY, h:mm:ss a'));
+	$('#temperature').
+		html(`${data.main.temp}°C`);
+	$('#description').
+		text(data.weather[0].description);
+	$('#wind-speed').
+		html(`Wind Speed: ${data.wind.speed} m/s`);
+	$('#weather-icon').
+		attr('src',
+			`...`);
+	$('#weather-info').fadeIn();
+}
